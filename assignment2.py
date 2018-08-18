ab2e4eda43fb7adf185935f113fc7579293cc72d
lonewolf39 <nikhilthakur3939@gmail.com>
2018-08-18 23:15:17 +0530

    This is assignment 2

---
+++ assignment2.py
@@ -0,0 +1,31 @@
+# Q.1- Print Something
+print("Hello Sir Assignment Check Krlo")
+
+# Q.2- Concatinate Strings
+a=("Hello sir ")
+b=("Assignment Check Krlo")
+print(a+b)
+
+# Q.3- Print 3 User Defined Variables
+x=int(input("Enter first Variable: "))
+y=int(input("Enter second Variable: "))
+z=int(input("Enter third Variable: "))
+print(x)
+print(y)
+print(z)
+
+# Q.4- Print Let's Get Started
+x=("Let's Get Started")
+print(x)
+
+# Q.5- Print The Given Values Using Placeholders
+x=('LoneWolf')
+y=('King')
+z=(3939)
+print("Name: %s , Aim: %s , Age: %d" %(x,y,z))
+
+# Q.6- Find The area of circle with user defined Radius
+r=float(input("enter the radius"))
+pi=3.14
+a=pi*(r**2)
+print("area of circle:" ,a)
